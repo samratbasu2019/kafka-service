d6d119d4760cf4022f0ecd0ce60857115d91fd9c
Samrat <samtina.basu@gmail.com>
2020-06-03 15:01:22 +0530

    Baseline

---
+++ src/main/java/com/infy/org/kafka/KafkaServiceStarter.java
@@ -0,0 +1,13 @@
+package com.infy.org.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaServiceStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaServiceStarter.class, args);
+	}
+
+}
