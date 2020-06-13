1da0bf342c2e169ddbe48eaa84b70fce014d1cf5
Samrat <samtina.basu@gmail.com>
2020-06-13 21:13:06 +0530

    Updated

--- src/main/java/com/infy/org/kafka/controller/KafkaController.java
+++ src/main/java/com/infy/org/kafka/controller/KafkaController.java
@@ -1,11 +1,13 @@
 package com.infy.org.kafka.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +17,18 @@ import com.infy.org.kafka.utility.Constant;
 @RestController
 @RequestMapping(value = "/kafka")
 public class KafkaController {
+	
+	private static final Logger log = LoggerFactory.getLogger(KafkaController.class);
 	private final Producer producer;
-
+	
 	@Autowired
 	public KafkaController(Producer producer) {
 		this.producer = producer;
 	}
 
 	@PostMapping(value = "/publish")
-	public ResponseEntity<?> sendMessageToKafkaTopic(@RequestParam("message") String message, @RequestParam("type") String type) {
+	public ResponseEntity<?> sendMessageToKafkaTopic(@RequestBody String message, @RequestParam("type") String type) {
+		log.info("Message came for publish is :"+message);
 		if (type.equalsIgnoreCase(Constant.APPRECIATION))
 			this.producer.sendAppreciationMessage(message);
 		else if(type.equalsIgnoreCase(Constant.FEEDBACK))
